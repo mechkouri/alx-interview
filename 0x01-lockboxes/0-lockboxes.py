7b29f7d547fd8d294dbe3c5610ba2cca68da13e9
mechkouri <saidmechkouri@gmail.com>
2024-07-02 12:00:29 +0000

    file updated

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
+"""
+Solution to lockboxes problem
+"""
+
+
 def canUnlockAll(boxes):
-    n = len(boxes)
-    keys = {0}
-    opened_boxes = set()
-    queue = [0]
+    """
+    Determines whether a series of locked boxes can be opened
+    based on keys that can be attained.
+    Solution to the lockboxes problem
+    """
+    if (type(boxes)) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
 
-    while queue:
-        box = queue.pop(0)
-        if box not in opened_boxes:
-            opened_boxes.add(box)
-            for key in boxes[box]:
-                if key not in keys:
-                    keys.add(key)
-                if key < n and key not in opened_boxes:
-                    queue.append(key)
-
-    return len(opened_boxes) == n
-
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
