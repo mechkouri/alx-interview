2fdfa29f94111dd3872cc3314d4d1ace2613535c
SAID MECHKOURI <66645279+mechkouri@users.noreply.github.com>
2024-07-04 14:59:50 +0100

    Update main_0.py

--- 0x01-lockboxes/main_0.py
+++ 0x01-lockboxes/main_0.py
@@ -1,1 +1,12 @@
-main_0.py
+def canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked = set([0])  # The first box is already unlocked
+    keys = set(boxes[0])  # Keys from the first box
+
+    while keys:
+        new_key = keys.pop()
+        if new_key < n and new_key not in unlocked:
+            unlocked.add(new_key)
+            keys.update(boxes[new_key])
+    
+    return len(unlocked) == n
