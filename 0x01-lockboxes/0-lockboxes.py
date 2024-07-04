a38437b28dfe3b8fe7451f05ca52cd13f64a887a
SAID MECHKOURI <66645279+mechkouri@users.noreply.github.com>
2024-07-04 15:01:51 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,20 @@
+#!/usr/bin/python3
+"""
+Module for the canUnlockAll function.
+"""
+
+
 def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+
+    Args:
+    boxes (list of lists): A list of lists where each inner list represents a box
+                           and contains keys to other boxes.
+
+    Returns:
+    bool: True if all boxes can be opened, False otherwise.
+    """
     n = len(boxes)
     unlocked = set([0])  # The first box is already unlocked
     keys = set(boxes[0])  # Keys from the first box
@@ -10,3 +26,14 @@ def canUnlockAll(boxes):
             keys.update(boxes[new_key])
     
     return len(unlocked) == n
+
+
+if __name__ == "__main__":
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))
