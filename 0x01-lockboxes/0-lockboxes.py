f915bcd1d1db885d762ea5b32be166c2e77f442e
SAID MECHKOURI <66645279+mechkouri@users.noreply.github.com>
2024-07-04 14:57:48 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,26 +1,12 @@
-#!/usr/bin/python3
-"""
-Solution to lockboxes problem
-"""
-
-
 def canUnlockAll(boxes):
-    """
-    Determines whether a series of locked boxes can be opened
-    based on keys that can be attained.
-    Solution to the lockboxes problem
-    """
-    if (type(boxes)) is not list:
-        return False
-    elif (len(boxes)) == 0:
-        return False
+    n = len(boxes)
+    unlocked = set([0])  # The first box is already unlocked
+    keys = set(boxes[0])  # Keys from the first box
 
-    for k in range(1, len(boxes) - 1):
-        boxes_checked = False
-        for idx in range(len(boxes)):
-            boxes_checked = k in boxes[idx] and k != idx
-            if boxes_checked:
-                break
-        if boxes_checked is False:
-            return boxes_checked
-    return True
+    while keys:
+        new_key = keys.pop()
+        if new_key < n and new_key not in unlocked:
+            unlocked.add(new_key)
+            keys.update(boxes[new_key])
+    
+    return len(unlocked) == n
