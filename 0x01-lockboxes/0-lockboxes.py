6070212e93fb8c70f0eb4079aa9f39345e5221b3
SAID MECHKOURI <66645279+mechkouri@users.noreply.github.com>
2024-07-04 20:44:42 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
+
 """
-Module for canUnlockAll method.
+Problem: You have n number of locked boxes in front of you.
+         Each box is numbered sequentially from 0 to n - 1
+         and each box may contain keys to the other boxes.
+Task: Write a method that determines if all the boxes can be opened.
 """
+
 
 def canUnlockAll(boxes):
     """
-    Determines if all the boxes can be opened.
-    :param boxes: list of lists, where each sublist contains keys to other boxes
-    :return: True if all boxes can be opened, else False
+    Function that checks with boolean value if the list type and
+    length to invoke two for iterations one to traverse the list
+    and the other to compaer if key is idx or not in order to open
     """
-    n = len(boxes)
-    unlocked = [False] * n
-    unlocked[0] = True
-    keys = [0]
-
-    while keys:
-        current_key = keys.pop()
-        for key in boxes[current_key]:
-            if key < n and not unlocked[key]:
-                unlocked[key] = True
-                keys.append(key)
-
-    return all(unlocked)
+    if type(boxes) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
